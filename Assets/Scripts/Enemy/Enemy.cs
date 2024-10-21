636da9d0c71fff4ec860d0498c0e3c1e3bcf78dd
ataerena <ataerena599@gmail.com>
2024-10-21 23:05:33 +0300

    enemy path finding, it sucks for now

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,21 +8,65 @@ public abstract class Enemy : MonoBehaviour
     private int currentHealth;
     protected EnemyState enemyState;
     protected EnemyState lastenemyState;
-    protected CapsuleCollider2D collider2D;
+    protected CapsuleCollider2D cldr2D;
     protected Rigidbody2D rb;
+    protected virtual float moveSpeed { get; set; } = 1f;
+    private bool movingRight;
+    [SerializeField] Transform hero;
+    [SerializeField] LayerMask playerLayer;
+    private float chaseRange { get; set; } = 5f;
     void Start()
     {
         enemyState = EnemyState.Idle;
         currentHealth = MaxHealth;
-        collider2D = GetComponent<CapsuleCollider2D>();
+        cldr2D = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (enemyState == EnemyState.Idle) {
-            PlayAnimation("Idle");
+        DetectHero();
+        UpdateAnimation();
+    }
+
+    protected virtual void DetectHero()
+    {
+        if (!CanDetectHero()) return;
+
+        bool facingRight = CheckIsFacingRight();
+        Vector2 movePoint = facingRight ? new Vector2(hero.position.x - 1f, hero.position.y) : new Vector2(hero.position.x + 1f, hero.position.y);
+
+        bool heroDetected = Physics2D.Raycast(transform.position, facingRight ? Vector3.right : Vector3.left, 7.5f, playerLayer);
+        if (heroDetected && transform.position.x != movePoint.x)
+        {
+            Debug.Log("LESS THAN!");
+            enemyState = EnemyState.Running;
+            transform.position = Vector2.MoveTowards(transform.position, movePoint, moveSpeed * Time.deltaTime);
         }
+        else
+        {
+            /* Debug.Log("Hero is NOT in range!"); */
+            ResetToIdle();
+        }
+    }
+
+    protected virtual bool CheckIsFacingRight()
+    {
+        if (transform.rotation == Quaternion.Euler(0, 0, 0)) 
+        {
+            return true;
+        }
+        else
+        {
+            movingRight = false;
+            return false;
+        }
+    }
+
+    protected virtual void Flip()
+    {
+        movingRight = !movingRight;
+        transform.rotation = Quaternion.Euler(0, movingRight ? 0 : 180, 0);
     }
 
     public virtual void TakeDamage(int _damage) // public class instead of protected so that the Hitbox script can access it
@@ -32,7 +76,9 @@ public abstract class Enemy : MonoBehaviour
         {
             currentHealth -= _damage;
             PlayAnimation("TakeHit");
-        } else {
+        } 
+        else 
+        {
             Die();
         }
     }
@@ -41,7 +87,19 @@ public abstract class Enemy : MonoBehaviour
     {
         PlayAnimation("Die");
         rb.isKinematic = true;
-        collider2D.enabled = false;
+        cldr2D.enabled = false;
+    }
+
+    protected virtual void UpdateAnimation()
+    {
+        if (enemyState == EnemyState.Idle) 
+        {
+            PlayAnimation("Idle");
+        }
+        else if (enemyState == EnemyState.Running)
+        {
+            PlayAnimation("Run");
+        }
     }
 
     protected virtual void PlayAnimation(string _name)
@@ -59,11 +117,17 @@ public abstract class Enemy : MonoBehaviour
         enemyState = EnemyState.Idle;
     }
 
+    protected virtual bool CanDetectHero()
+    {
+        return enemyState == EnemyState.Idle || enemyState == EnemyState.Running;
+    }
+
     protected enum EnemyState
     {
         Idle,
         TakingDamage,
         Attacking,
+        Running,
         Dead
     }
 }
--- Assets/Scripts/Enemy/Goblin.cs
+++ Assets/Scripts/Enemy/Goblin.cs
@@ -1,4 +1,4 @@
 public class Goblin : Enemy
 {
-    
+    protected override float moveSpeed { get; set; } = 1f;
 }
